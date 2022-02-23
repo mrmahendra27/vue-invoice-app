2a873215e6fa6dd8c62fcdf1211666ba77635379
Mahendra Singh Khati <60776546+mrmahendra27@users.noreply.github.com>
2022-02-24 00:14:42 +0530

    first commit

---
+++ src/components/Navigation.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="flex">
+    <div class="branding flex">
+      <img src="@/assets/file-invoice-dollar-solid.png" alt="logo" />
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Navigation'
+}
+</script>
+
+<style>
+    
+</style>
